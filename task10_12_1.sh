f2b1c3b01a45262298de262b5cf726c0799004fc
illia <garterfild14@gmail.com>
2018-05-19 09:04:40 +0300

    last update

--- task10_12_1.sh
+++ task10_12_1.sh
@@ -4,9 +4,9 @@ export dir="$(cd "$(dirname "$0")" && pwd)"
 export MAC=52:54:00:`(date; cat /proc/interrupts) | md5sum | sed -r 's/^(.{6}).*$/\1/; s/([0-9a-f]{2})/\1:/g; s/:$//;'`
 
 #Exporting variables from config
-export $(grep -v '#' $dir/templates/config)
-envsubst < $dir/templates/config > $dir/config
 export $(grep -v '#' $dir/config)
+envsubst < $dir/config > $dir/exec_config
+export $(grep -v '#' $dir/exec_config)
 #Creating directories
 
 mkdir -p $(echo $SSH_PUB_KEY | rev | cut -c12- | rev)
